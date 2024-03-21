f4addf23b19b738cf37398a778c5e1c98708cfb2
Asia <ziola.jm@gmail.com>
2024-03-21 15:29:39 +0100

    season1 is on

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -7,7 +7,9 @@ import ButtonWithLink from '../../components/ButtonWithLink/ButtonWithLink';
 
 interface HeaderProps {
   walletAddress: string | null;
-  setWalletAddress: (v: string | null | ((prev?: string | null) => string | null)) => string | null;
+  setWalletAddress: (
+    v: string | null | ((prev?: string | null) => string | null)
+  ) => string | null;
   connect: () => void;
   disconnect: () => void;
   timestamp: number | null;
@@ -17,60 +19,77 @@ interface HeaderProps {
 const Header: Component<HeaderProps> = (props) => {
   return (
     <Container fluid>
-      <Row class='linkbar'>
-        <Col class='d-flex align-middle justify-content-center'>
+      <Row class="linkbar">
+        <Col class="d-flex align-middle justify-content-center">
           <div>
-            Join our Discord to connect your wallet address to your profile and mine RSG through activity on the server
-            -{' '}
-            <a href='https://discord.gg/redstonedefi' target='_blank' class='linkbar__link'>
+            Join our Discord to connect your wallet address to your profile and
+            mine RSG through activity on the server -{' '}
+            <a
+              href="https://discord.gg/redstonedefi"
+              target="_blank"
+              class="linkbar__link"
+            >
               LINK
             </a>
           </div>
           <div></div>
         </Col>
       </Row>
-      <Row class='header'>
-        <Col class='header__nav'>
-          <div class='header__nav__logo'>
-            <img src='/assets/header-logo.svg' />
+      <Row class="header">
+        <Col class="header__nav">
+          <div class="header__nav__logo">
+            <img src="/assets/header-logo.svg" />
           </div>
-          <div class='header__nav__button'>
+          <div class="header__nav__button">
             {props.walletAddress ? (
-              <div class='d-flex align-items-end align-items-md-center flex-column-reverse flex-md-row'>
-                <div class='header__nav__button__result pt-1 mt-md-0'>
+              <div class="d-flex align-items-end align-items-md-center flex-column-reverse flex-md-row">
+                <div class="header__nav__button__result pt-1 mt-md-0">
                   {props.walletAddress.substr(0, 3) +
                     '...' +
                     props.walletAddress.substr(props.walletAddress.length - 3)}
                 </div>
-                <Button color='primary' handleClick={props.disconnect} loading={props.loadingWalletAddress}>
+                <Button
+                  color="primary"
+                  handleClick={props.disconnect}
+                  loading={props.loadingWalletAddress}
+                >
                   Disconnect wallet
                 </Button>
               </div>
             ) : (
-              <Button color='primary' handleClick={props.connect} loading={props.loadingWalletAddress}>
+              <Button
+                color="primary"
+                handleClick={props.connect}
+                loading={props.loadingWalletAddress}
+              >
                 Connect wallet
               </Button>
             )}
           </div>
         </Col>
-        <div class='header__main'>
-          <div class='header__main__subtitle'>Launch</div>
-          <div class='header__main__title'>
+        <div class="header__main">
+          <div class="header__main__subtitle">Launch</div>
+          <div class="header__main__title">
             RedStone
             <br />
             Expedition
           </div>
-          <div class='header__main__season__title'>Season 1 coming soon</div>
+          <div class="header__main__season__title">Season 1</div>
           <Show
             when={props.timestamp != null}
-            fallback={<img class='header__main__season__img' src='/assets/diamond.svg' />}
+            fallback={
+              <img
+                class="header__main__season__img"
+                src="/assets/diamond.svg"
+              />
+            }
           >
             <Ticker timestamp={props.timestamp as number} />
           </Show>
-          <div class='header__main__links'>
+          <div class="header__main__links">
             <ButtonWithLink
-              color='outline'
-              link='https://blog.redstone.finance/2023/10/31/redstone-is-launching-expedition'
+              color="outline"
+              link="https://blog.redstone.finance/2023/10/31/redstone-is-launching-expedition"
             >
               Learn more
             </ButtonWithLink>
