6a6f00cb113a4e24dfcf6c8c99ddf932b33adaa0
Asia <ziola.jm@gmail.com>
2024-08-09 17:19:17 +0200

    layout fixes

--- src/components/RowTable/RowTable.tsx
+++ src/components/RowTable/RowTable.tsx
@@ -33,10 +33,7 @@ const RowTable: Component<RowTableProps> = (props) => {
           {props.header}
         </Col>
       </Row>
-      <Row>
-        <div class="row-table__switch">All Time</div>
-      </Row>
-      <Table responsive="sm">
+      <Table responsive="sm" class="pt-5">
         <thead>
           <tr class="table__headers">
             <th></th>
