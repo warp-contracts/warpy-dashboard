d14962eebe7c96f4de9e1849dbbdbdc79b5b953b
Asia <ziola.jm@gmail.com>
2024-09-02 09:37:56 +0200

    remove memethon and venus slides

--- src/layouts/Main/Main.tsx
+++ src/layouts/Main/Main.tsx
@@ -95,24 +95,6 @@ const Main: Component<MainProps> = (props) => {
           <ButtonCardWrapper withSlider={true}>
             <div class="slide slide1">
               <ButtonCard
-                title="Memethon"
-                buttonTitle="Participate"
-                link="https://x.com/redstone_defi/status/1828432534381985833"
-                subtitle=""
-                backgroundImage="/assets/memethon.png"
-              ></ButtonCard>
-            </div>
-            <div class="slide slide2">
-              <ButtonCard
-                title="Venus On-chain Quest"
-                buttonTitle="Participate"
-                link="https://x.com/redstone_defi/status/1826620424513618195"
-                subtitle=""
-                backgroundImage="/assets/venus.png"
-              ></ButtonCard>
-            </div>
-            <div class="slide slide3">
-              <ButtonCard
                 title="Weekly Best Content"
                 buttonTitle="Participate"
                 link="https://discord.com/channels/786251205008949258/1206919012588585001/1206919012588585001"
@@ -120,7 +102,7 @@ const Main: Component<MainProps> = (props) => {
                 backgroundImage="/assets/background_weekly.png"
               ></ButtonCard>
             </div>
-            <div class="slide slide4">
+            <div class="slide slide2">
               <ButtonCard
                 title="Hall of Fame"
                 buttonTitle="Participate"
@@ -129,7 +111,7 @@ const Main: Component<MainProps> = (props) => {
                 backgroundImage="/assets/background_hof.png"
               ></ButtonCard>
             </div>
-            <div class="slide slide5">
+            <div class="slide slide3">
               <ButtonCard
                 title="RedStone Miners"
                 buttonTitle="Participate"
